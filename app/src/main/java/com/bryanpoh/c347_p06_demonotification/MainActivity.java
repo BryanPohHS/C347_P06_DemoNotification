da153542d06702892f6ddf845765a648c55c0533
Bryan Poh <zandrex00@gmail.com>
2020-05-29 11:23:11 +0800

    Initial commit

---
+++ app/src/main/java/com/bryanpoh/c347_p06_demonotification/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bryanpoh.c347_p06_demonotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
